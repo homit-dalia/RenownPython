68979127f60bda87513775afc7373c12861d352f
Homit Dalia <homitdalia59@gmail.com>
2023-08-05 02:20:44 +0530

    Login, Display, Update

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import os
 from flask import Flask, request
 import pymysql
 import re
+import jwt
+import datetime
+import random
+from twilio.rest import Client
+
 
 db_user = os.environ.get('CLOUD_SQL_USERNAME')
 db_password = os.environ.get('CLOUD_SQL_PASSWORD')
@@ -9,7 +14,23 @@ db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
 db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = str(os.environ.get('SECRET_KEY'))
 
+twilio_account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+twilio_auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+
+def send_otp(mobile):
+    try:
+        password = random.randint(100000, 999999)
+        client = Client(twilio_account_sid, twilio_auth_token)
+        client.messages.create(
+            body=f"Your OTP is {password}",
+            from_='+17756180651',
+            to=f'+91{mobile}'
+        )
+        return password
+    except Exception as e:
+        return f"Error sending OTP - {e}"  
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
@@ -20,17 +41,33 @@ def main():
         host = '127.0.0.1'
         cnx = pymysql.connect(user=db_user, password=db_password,host=host, db=db_name)
 
-    try:
-        req_type = request.args["type"]
+    req_type = request.args.get("type", None)
+    if req_type is None:
+        return "Type of operation required. Please verify that the 'type' is in the URL."
+    else:
         print("Printing Request Type")
         print(req_type)
-    except:
-        return "Type of operation required. Please verify that the 'type' is in the URL."
 
+    def display():
+        try:
+            resp = {}
+            arr = []
+            with cnx.cursor() as cursor:
+                cursor.execute('select * from users;')
+                result = cursor.fetchall()
+                for row in result:
+                    arr.append(row[0:5])
+            resp['count'] = len(arr)
+            resp['users'] = arr
+            return resp
+        except Exception as e:
+            return f'No data found :(. {e}'
+    
+    if(req_type == "display"):
+        return display()
+    
     try:
         data = request.get_json()
-        print("Printing Data")
-        print(data)
     except:
         return "Missing data. Please verify that the data is in JSON format."
     
@@ -41,43 +78,110 @@ def main():
             mobile = data['mobile']
             email = data['email']
         except:
-            return "Missing SignUp Values. Please verify that username, mobile, and email exists with the request."
+            return "Missing SignUp Values. Please verify that username, mobile, and email are sent with the request."
         
-        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
-        if not re.match(regex, email):
+        if(len(username) < 4 or len(username) > 20):
+            return "Invalid username. Please enter a username that contains 4 - 20 characters."
+        
+        email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+        if not re.match(email_regex, email):
             return "Invalid email address. Please verify that the email is valid."
+        
+        if(len(mobile) != 10):
+            return "Invalid mobile number. Please verify that the mobile number is valid."
         
         try:
             with cnx.cursor() as cursor:
-                cursor.execute(f"SELECT username FROM user WHERE username = '{username}';")
+                cursor.execute(f"SELECT username FROM users WHERE username = '{username}';")
 
                 if(cursor.rowcount > 0):
                     return "Username already exists. Please signup with a different username."
 
-                cursor.execute(f"INSERT INTO user (username, mobile, email, fullname) VALUES ('{username}', '{mobile}', '{email}', '');")
+                cursor.execute(f"INSERT INTO users (username, mobile, email, fullname, password) VALUES ('{username}', '{mobile}', '{email}', '', -1);")
+                password = send_otp(mobile)
+                cursor.execute(f"UPDATE users SET password = '{password}' WHERE username = '{username}';")
                 cnx.commit()
                 return "User added successfully."
-        except:
-            return "Error adding new user."
+        except Exception as e:
+            return f"Error adding new user - {e}"
         
-
     def login():
-        pass
-
-    def update():
-        pass
-
-    def display():
+        cursor = cnx.cursor()
         try:
-            arr = []
-            with cnx.cursor() as cursor:
-                cursor.execute('select * from user;')
-                result = cursor.fetchall()
-                for row in result:
-                    arr.append(row)
+            username = data.get('username', None)
+            if username is None:
+                return "Missing Login Values. Please verify that username exists with the request."
 
-        except:
-            current_msg = 'No data found :('
+            user = []
+            mobile = ""
+
+            cursor.execute(f"SELECT * FROM users WHERE username = '{username}';")
+            result = cursor.fetchone()
+
+            if(cursor.rowcount == 0):
+                return "Username does not exist. Please signup first."
+
+            for row in result:
+                user.append(row)
+            mobile = user[2]
+            
+            resend = data.get('resend', None)
+            if (resend == "True"):
+                try:
+                    cursor.execute(f"SELECT mobile FROM users WHERE username = '{username}';")
+                    password = send_otp(mobile)
+                    cursor.execute(f"UPDATE users SET password = '{password}' WHERE username = '{username}';")
+                    cnx.commit()
+                    return f"OTP sent to your number ending with ******{user[2][-4:]}"
+                except Exception as e:
+                    return f"Error sending or storing OTP - {e}"
+
+            otp = data.get('otp', None)
+            if otp is None or otp == -1:
+                return "Missing OTP Value. Please verify that otp exists with the request."
+            
+            cursor.execute(f"SELECT * FROM users WHERE username = '{username}';")
+            result = cursor.fetchone()
+            
+
+            user = []
+            for row in result:
+                user.append(row)
+            password = user[5]
+
+            if(otp != str(password)):
+                return "Invalid OTP. Please renter OTP."
+            
+            token = jwt.encode(payload={"username": username, "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, key=app.config['SECRET_KEY'], algorithm='HS256')
+            return str(token)
+            
+        except Exception as e:
+            return f"Error Logging in - {e}"
+        
+    def update():
+        try:
+            token = data.get('token', None)
+            if token is None:
+                return "Missing Token."
+            token_data = jwt.decode(jwt=token, key=app.config['SECRET_KEY'], algorithms=["HS256"])
+            username = token_data.get('username', None)
+            if username is None:
+                return "Missing Username with Token."
+            
+            fullname = data.get('fullname', None)
+            if(fullname is None or len(fullname) < 3):
+                return "Invalid fullname. Please enter a fullname that contains at least 3 characters."
+
+            with cnx.cursor() as cursor:
+                cursor.execute(f"UPDATE users SET fullname = '{fullname}' WHERE username = '{username}';")
+                cursor.execute(f"UPDATE users SET password = -1 WHERE username = '{username}';")
+                cnx.commit()
+                return "User updated successfully"
+            
+        except Exception as e:
+            return f"Error Updating user - {e}"
+
+
 
     if req_type == "signup":
         return signup()
@@ -85,12 +189,9 @@ def main():
         return login()
     elif req_type == "update":
         return update()
-    elif req_type == "display":
-        return display()
-    
-    cnx.close()
+    else:
+        return "Invalid type of operation. Please verify that the 'type' is either signup, login, update or display."
 
-    return True
 
 
 if __name__ == '__main__':
